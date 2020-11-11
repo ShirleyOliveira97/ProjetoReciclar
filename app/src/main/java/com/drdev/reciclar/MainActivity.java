38149f09763d581d5696a3895888d553490f0154
dralv3s <dralv3s@gmail.com>
2020-11-11 20:16:09 -0300

    Initial commit

---
+++ app/src/main/java/com/drdev/reciclar/MainActivity.java
@@ -0,0 +1,14 @@
+package com.drdev.reciclar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
