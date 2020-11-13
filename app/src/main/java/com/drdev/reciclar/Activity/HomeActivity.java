115ff017e39fbb02a3ac15dfeb548051f53d9507
dralv3s <dralv3s@gmail.com>
2020-11-13 00:37:12 -0300

    Inclusao de servicos de banco ed dados, servicos auxiliares. Finalização do fluxo de cadastro e login.

--- app/src/main/java/com/drdev/reciclar/Activity/HomeActivity.java
+++ app/src/main/java/com/drdev/reciclar/Activity/HomeActivity.java
@@ -1,14 +1,16 @@
-package com.drdev.reciclar;
+package com.drdev.reciclar.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.drdev.reciclar.R;
+
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
